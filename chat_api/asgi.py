52dd4e3ac07e715be41cd1476a0357774e5d5d68
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-19 11:03:25 -0500

    hoping https errors can be fixed this easily but know i probably need to add a package

--- chat_api/asgi.py
+++ chat_api/asgi.py
@@ -21,6 +21,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "https": django_asgi_app,
     'websocket' : AuthMiddlewareStack(
         URLRouter(
            chat.routing.websocket_urlpatterns 
