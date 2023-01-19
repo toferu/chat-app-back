f3cb20a775e327d558a9c743ea6f0c257577c7f1
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-19 14:39:34 -0500

    reverting to state before TLS attempts

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
 ]
