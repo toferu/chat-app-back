b9ed5c8d9ea222878c962c997e90a6afdd78206e
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-19 22:34:11 -0500

    commented out some unnecessary imports in the asgi.py

--- chat_api/asgi.py
+++ chat_api/asgi.py
@@ -13,8 +13,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 # from channels.routing import get_default_application
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-from django.urls import path
+# from django.core.asgi import get_asgi_application
+# from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_api.settings')
 
