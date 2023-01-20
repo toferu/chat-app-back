bc42ff7a9c8508e2179c2df97a625924ce66756a
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-19 22:37:11 -0500

    removed garbage from the end of my routing.py file

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()) is not None,
+    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
 ]
